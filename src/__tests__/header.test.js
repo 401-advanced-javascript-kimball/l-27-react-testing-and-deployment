52f4b74f04eca7edb726f3a0e3b419da3af08956
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2019-11-20 12:49:47 -0800

    updated package.json and package-lock.json and started work on tests

---
+++ src/__tests__/header.test.js
@@ -0,0 +1,9 @@
+import Header from '../components/header';
+
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+import App from '../app';
+
+// describe('');
