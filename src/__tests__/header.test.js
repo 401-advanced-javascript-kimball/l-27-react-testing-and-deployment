cba0f92c56f5d157a8a0f3c085a386f5b2547a1e
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2019-11-20 12:55:16 -0800

    updated tests

--- src/__tests__/header.test.js
+++ src/__tests__/header.test.js
@@ -1,4 +1,4 @@
-import Header from '../components/header';
+import Header from '../components/header/header';
 
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
